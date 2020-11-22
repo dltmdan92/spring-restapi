406621803537f84f7bf86dad123e6ec8233dcf04
dltmdan92 <dltmdan92@naver.com>
2020-11-22 18:12:29 +0900

    @Valid를 통한 validation (BAD REQ 발생 시키기)

--- src/main/java/com/seungmoo/springrestapi/events/EventController.java
+++ src/main/java/com/seungmoo/springrestapi/events/EventController.java
@@ -1,19 +1,18 @@
 package com.seungmoo.springrestapi.events;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Controller
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
@@ -37,15 +36,25 @@ public class EventController {
      * Location Header에 생성된 이벤트를 조회할 수 있는 URI 담겨준다.
      * id는 DB에 들어갈 때 자동생성된 값으로 나온다.
      *
-     * <입력값 제한하는 메소드>
+     * <unknown 입력 값 무시하는 메소드>
      *     - 특정 입력값을 제외하고 ignore 한다.
      * API의 RequestParameter 입력값을 제한하기 위해
-     * EventDto 객체를 별도 생성하여 사용한다. (id, free 여부 값 등은 파라미터로 받아선 안된다.)
+     * EventDto 객체를 "별도 생성"하여 사용한다. (id, free 여부 값 등은 파라미터로 받아선 안된다.)
+     * 그리고 EventDto 객체를 Event로 매핑해서 사용하면 된다. (objectMapper 사용)
+     *
+     * <unknown 입력 값 있을 시 Bad Request 발생>
+     *     1. API RequestParameter에 @Valid 선언해주기 --> spring-boot-starter-validation을 꼭 pom.xml에 선언해준다.
+     *          Valid의 수행결과는 바로 옆에 선언된 Errors 파라미터에 포워딩 된다.
+     *     2. 
+     *
      * @return
      */
     @PostMapping
-    public ResponseEntity<?> createEvent(@RequestBody EventDto eventDto) {
-
+    public ResponseEntity<?> createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+        // @Valid에서 발생한 Error의 결과가 Errors로 넘겨진다.
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
         // EventDto 객체를 --> Event 객체로 옮겨보자 (ModelMapper를 통해 편하게 사용)
         Event event = modelMapper.map(eventDto, Event.class);
 
--- src/test/java/com/seungmoo/springrestapi/events/EventControllerTestNonSlicing.java
+++ src/test/java/com/seungmoo/springrestapi/events/EventControllerTestNonSlicing.java
@@ -107,4 +107,14 @@ public class EventControllerTestNonSlicing {
                 .andDo(print())
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void createEvent_Bad_Req_Empty_input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
 }
