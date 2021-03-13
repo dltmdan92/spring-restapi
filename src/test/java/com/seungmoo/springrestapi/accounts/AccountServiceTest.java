13ca62d759a23d4e2af789b2d77a79ed24257bac
seungmoo.lee <seungmoo.lee@lotte.net>
2021-03-13 11:19:43 +0900

    예외 테스트(로그인 실패)

--- src/test/java/com/seungmoo/springrestapi/accounts/AccountServiceTest.java
+++ src/test/java/com/seungmoo/springrestapi/accounts/AccountServiceTest.java
@@ -1,15 +1,19 @@
 package com.seungmoo.springrestapi.accounts;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -21,6 +25,7 @@ class AccountServiceTest {
     @Autowired
     AccountRepository accountRepository;
 
+    @DisplayName("로그인 인증 성공 테스트")
     @Test
     void findByUsername() {
         String username = "seungmoo@gmail.com";
@@ -39,6 +44,12 @@ class AccountServiceTest {
 
         // Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @DisplayName("로그인 인증 실패 테스트 - 잘못된 username")
+    @Test
+    void findByUsernameFail() {
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> accountService.loadUserByUsername("random@random.com"));
     }
 
 }
